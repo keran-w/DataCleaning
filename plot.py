10bdca3fa4a1d5e41ea6f2b4267ac5b27274f21b
keran-w <83668340+keran-w@users.noreply.github.com>
2022-05-07 16:03:50 +0800

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib_inline import backend_inline
+
+from IPython import display
+
+
+def plot_config(chinese=False):
+    backend_inline.set_matplotlib_formats('svg')
+    sns.set(style="whitegrid")
+    sns.set_color_codes("pastel")
+    plt.rcParams['axes.unicode_minus'] = False
+    if chinese:
+        plt.rcParams['font.sans-serif'] = ['SimHei']
+        sns.set(font='SimHei')
+
+
+def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+    """Set the axes for matplotlib"""
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.set_xscale(xscale)
+    axes.set_yscale(yscale)
+    axes.set_xlim(xlim)
+    axes.set_ylim(ylim)
+    if legend:
+        axes.legend(legend)
+    axes.grid()
+
+
+def plot_correlation(df, chinese=False, fillna=-1, decimal=4):
+    plot_config(chinese)
+    sns.heatmap(df.corr().fillna(fillna), annot=True,
+                cmap='Blues', fmt=f'.{decimal}g')
+    plt.show()
