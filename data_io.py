d1a388c0f6bea06496c3c4011b7765b2a6db6042
keran-w <83668340+keran-w@users.noreply.github.com>
2022-05-23 14:26:40 +0800

    Update data_io.py

--- data_io.py
+++ data_io.py
@@ -1,7 +1,7 @@
 from utils import *
 
 
-def read_file(filename: str, columns=None, sheetid=1, sep=','):
+def read_file(filename: str, columns=None, sheetid=1, sep=',', use_fast=False) -> Union[pd.DataFrame, List[str]]:
     """ 读取多种文件类型的数据, 对于列表数据, 可以选择部分表头
 
     Args:
@@ -9,10 +9,12 @@ def read_file(filename: str, columns=None, sheetid=1, sep=','):
         columns (List[str], optional): 选择表格数据希望保留的列. Defaults to None.
         sheetid (int, optional): 对于xlsx及xls文件, 选择需要读取的表格是该文件的第几个表格. Defaults to 1.
         sep (str, optional): csv及txt文件的分割符. Defaults to ','.
+        use_fast (bool, optional): 是否首先将xlsx文件转为csv文件, 这样会对xlsx文件的读取提速. Defaults to False.
 
     Returns:
-        pd.DataFrame: 读取文件的表格数据, 如果输入文件的文件名为csv, xlsx或xls
-    或  List[str]: 读取txt文件的字符串列表
+        Union[pd.DataFrame, List[str]]: 
+            读取文件的表格数据, 如果输入文件的文件名为csv, xlsx或xls
+            或读取txt文件的字符串列表
 
     Examples:
         >>> read_file('data.csv', ['col_1', 'col_2'])
@@ -27,17 +29,23 @@ def read_file(filename: str, columns=None, sheetid=1, sep=','):
     if filetype == 'csv':
         data = pd.read_csv(filename, sep=sep)
     elif filetype == 'xlsx':
-        from xlsx2csv import Xlsx2csv
-        buffer = StringIO()
-        Xlsx2csv(filename, outputencoding='utf-8').convert(buffer, sheetid)
-        buffer.seek(0)
-        data = pd.read_csv(buffer)
+        if use_fast:
+            from xlsx2csv import Xlsx2csv
+            buffer = StringIO()
+            Xlsx2csv(filename, outputencoding='utf-8').convert(buffer, sheetid)
+            buffer.seek(0)
+            data = pd.read_csv(buffer)
+        else:
+            data = pd.read_excel(filename, sheetid)
+
     elif filetype == 'xls':
-        data = pd.read_excel(filename)
+        data = pd.read_excel(filename, sheetid)
     elif filetype == 'txt':
         f = open(filename, 'r', encoding='utf-8-sig')
         data = f.read().split(sep)
         f.close()
+    if data is None:
+        raise f'filetype <{filetype}> is not supported'
 
     if columns is not None and filetype in ('csv', 'xlsx', 'xls'):
         data = data[columns]
@@ -56,14 +64,14 @@ def save_file(data: Union[pd.DataFrame, List], destination: str, filetype: str =
         >>> save_file(df1, 'df1.csv')
         >>> save_file(df2, 'df2.csv', filetype='csv')
     """
-    
+
     if filetype is None:
         filetype = destination.split('.')[-1]
     elif destination.split('.')[-1] in ('csv', 'txt'):
         pass
     else:
         destination += f'.{filetype}'
-    
+
     if filetype == 'csv':
         data.to_csv(destination, index=False, encoding='utf-8-sig')
     elif filetype == 'txt':
